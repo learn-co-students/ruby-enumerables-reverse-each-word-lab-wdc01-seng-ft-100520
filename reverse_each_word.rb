9877cdd324f130d5668aa3fdef7b482942770c63
Justin Clifford <justoon7.jc@gmail.com>
2020-09-28 21:54:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
 def reverse_each_word(string)
   array = string.split
+  #assigning the array version of the string to a variable
   new_array = array.collect do |key|
+    #assigning the collect iteration of my string array to a new array and iterating through it to
     key.reverse
+    #collect the reverse order of each key in the string array into the the new array
   end
   new_array.join(" ")
+
+  #after collecting the new array i am joining the new array to converrt it back to a string and then it returns the new string as well
 end
